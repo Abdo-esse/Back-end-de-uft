296048900a797da339ec1f9150630aceca1439b1
abdelilah2005 <abdelilahessemlali@gmail.com>
2024-12-15 18:24:18 +0100

    j'ai temine la creation de database et tables

--- db.sql
+++ db.sql
@@ -1,9 +1,56 @@
 create database FUT;
  use FUT;
  create table club(
-    id PRIMARY key AUTO_INCREMENT not null,
+    id int PRIMARY key AUTO_INCREMENT not null,
     name VARCHAR(50),
-    image VARCHAR,
+    logo VARCHAR (200),
     city VARCHAR(50),
     country VARCHAR(50)
-    )
+    );
+ 
+
+ create table  Nationality(
+    id int PRIMARY key AUTO_INCREMENT not null,
+    name VARCHAR(50),
+    flage VARCHAR (200)
+    );
+
+
+    create table players(
+       id int PRIMARY key AUTO_INCREMENT not null,
+       name varchar(50),
+       position CHAR(5),
+       status char(15),
+       rating int not null,
+       date_delat date,
+       id_club int not null,
+       id_nationality int not NULL,
+       Foreign Key (id_club) REFERENCES club (id),
+       Foreign Key (id_nationality) REFERENCES Nationality (id)
+    ) ;
+ 
+    create table footballplayer(
+        id int not null PRIMARY key AUTO_INCREMENT,
+        pace int not null,
+        shooting int not null,
+        passing int not null,
+        dribbling int not null,
+        defending int not null,
+        physical int not null,
+        id_player int not null,
+        Foreign Key (id_player) REFERENCES players (id)
+    );
+
+    create table  golkeeperPlayer(
+        id int not null PRIMARY key AUTO_INCREMENT,
+        diving int not null,
+        handling int not null,
+        kicking int not null,
+        reflexes int not null, 
+        speed int not null,
+        physical int not null,
+        id_player int not null,
+        Foreign Key (id_player) REFERENCES players (id)
+    );
+ show TABLES;
+
