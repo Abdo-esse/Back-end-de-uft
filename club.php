c29613e8966609802a26bed6eb82c29a27fb0758
abdelilah2005 <abdelilahessemlali@gmail.com>
2024-12-17 22:59:13 +0100

    j'ai termin la fonction de delete

--- club.php
+++ club.php
@@ -69,7 +69,7 @@
 //   $run= mysqli_query($conn,$test);
    
 // }
-  
+
 
  $query="select * from club";
  $result=mysqli_query($conn,$query);
@@ -187,7 +187,7 @@
 </svg>
 
                   </a>
-                    <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline"><svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                    <a href="deletclub.php?delateid=<?php echo $row ['id']?>" class="font-medium text-blue-600 dark:text-blue-500 hover:underline"><svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
   <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m15 9-6 6m0-6 6 6m6-3a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
 </svg>
 
---
+++ deletclub.php
@@ -0,0 +1,13 @@
+<?php 
+     include_once("conectdb.php");
+     if(isset($_GET['delateid'])){
+          $id=$_GET['delateid'];
+          $query=mysqli_query($conn,"delete from club where id='$id'");
+          if($query){
+               
+               echo"<script>document.location='club.php';</script>";
+          }
+
+
+        }
+?>
